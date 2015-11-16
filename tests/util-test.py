c116f81d5900365fdaea889acd93105d9deb39d0
Alex Zai <azai91@gmail.com>
2015-11-15 18:36:57 -0800

    (feat) add util unit tests

--- tests/util-test.py
+++ tests/util-test.py
@@ -2,13 +2,30 @@
 Unit tests for util library
 """
 
+import json
 import src.util as util
 import unittest
 
 class TestUtil(unittest.TestCase):
 
     def test_validate_amount(self):
-        self.assertTrue(util.validate_amount('0'))
+        self.assertEqual(util.validate_amount('1'),'1.00')
+        self.assertEqual(util.validate_amount('10'),'10.00')
+        self.assertEqual(util.validate_amount('10.'),'10.00')
+        self.assertEqual(util.validate_amount('10.0'),'10.00')
+        self.assertEqual(util.validate_amount('10.00'),'10.00')
+        self.assertFalse(util.validate_amount('f'))
+        self.assertFalse(util.validate_amount('1f'))
+        self.assertFalse(util.validate_amount('10.f'))
+        self.assertFalse(util.validate_amount('10.0f'))
+
+    def test_create_post_message(self):
+        sample_friend = json.dumps({
+            'display_name' : 'Test',
+            'amount' : '12.00',
+            'note' : 'test'
+        })
+        self.assertEqual(str(util.create_post_message(sample_friend)),'Test received 12.00 for test')
 
 if __name__ == '__main__':
     unittest.main()
