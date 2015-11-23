e5afabd5c80c9ab54f60b59d013c66c0a4f9eb72
Alex Zai <azai91@gmail.com>
2015-11-22 22:39:19 -0800

    (feat) lint

--- tests/sample_data.py
+++ tests/sample_data.py
@@ -1,39 +1,49 @@
+"""
+Sample data for tests
+"""
+
+# pylint: disable=invalid-name
+
 sample_friends = [
     {
-     "username":"kortina",
-     "about":"make a joyful sound, la da da da",
-     "last_name":"Kortina",
-     "display_name":"Andrew Kortina",
-     "first_name":"Andrew",
-     "profile_picture_url":"https://venmopics.appspot.com/u/v5/s/25f9c7a0-5d5c-4988-8737-a3278d78ae42",
-     "id":"145434160922624167"
+        "username":"kortina",
+        "about":"make a joyful sound, la da da da",
+        "last_name":"Kortina",
+        "display_name":"Andrew Kortina",
+        "first_name":"Andrew",
+        "profile_picture_url":
+            "https://venmopics.appspot.com/u/v5/s/25f9c7a0-5d5c-4988-8737-a3278d78ae42",
+        "id":"145434160922624167"
     },
     {
-     "username":"iqram",
-     "about":"Frisbee enthusiast",
-     "last_name":"magdon-ismail - organic",
-     "display_name":"Iqram magdon-ismail - organic",
-     "first_name":"Iqram",
-     "profile_picture_url":"https://venmopics.appspot.com/u/v11/s/6c3740ad-26bd-475c-9da2-7cb3fd7591da",
-     "id":"145436736225280235"
+        "username":"iqram",
+        "about":"Frisbee enthusiast",
+        "last_name":"magdon-ismail - organic",
+        "display_name":"Iqram magdon-ismail - organic",
+        "first_name":"Iqram",
+        "profile_picture_url":
+            "https://venmopics.appspot.com/u/v11/s/6c3740ad-26bd-475c-9da2-7cb3fd7591da",
+        "id":"145436736225280235"
     },
     {
-     "username":"staub",
-     "about":"phonewalletbaby",
-     "last_name":"Staub",
-     "display_name":"Andrew Staub",
-     "first_name":"Andrew",
-     "profile_picture_url":"https://venmopics.appspot.com/u/v1/s/0b4b85ab-a72b-4326-a887-5bf7a08ff445",
-     "id":"152710129123328341"
+        "username":"staub",
+        "about":"phonewalletbaby",
+        "last_name":"Staub",
+        "display_name":"Andrew Staub",
+        "first_name":"Andrew",
+        "profile_picture_url":
+            "https://venmopics.appspot.com/u/v1/s/0b4b85ab-a72b-4326-a887-5bf7a08ff445",
+        "id":"152710129123328341"
     },
     {
-     "username":"azeem",
-     "about":"No Short Bio",
-     "last_name":"Ansar",
-     "display_name":"Azeem Ansar",
-     "first_name":"Azeem",
-     "profile_picture_url":"https://venmopics.appspot.com/u/v1/s/2b797ba9-1bd5-41b8-a5a9-f133e128f234",
-     "id":"161568197181440668"
+        "username":"azeem",
+        "about":"No Short Bio",
+        "last_name":"Ansar",
+        "display_name":"Azeem Ansar",
+        "first_name":"Azeem",
+        "profile_picture_url":
+            "https://venmopics.appspot.com/u/v1/s/2b797ba9-1bd5-41b8-a5a9-f133e128f234",
+        "id":"161568197181440668"
     }
 ]
 
@@ -47,7 +57,8 @@ sample_user = {
     "email": "coolguy12345@gmail.com",
     "phone": "5555555555",
     "about": "So happy",
-    "profile_picture_url": "https:\/\/venmopics.appspot.com\/u\/v3\/s\/6ecc7b37-5c4a-49df-b91e-3552f02dc397",
+    "profile_picture_url":
+        "https://venmopics.appspot.com/u/v3/s/6ecc7b37-5c4a-49df-b91e-3552f02dc397",
     "id": "1088551785594880949",
     "date_joined": "2013-02-10T21:58:05"
 }
