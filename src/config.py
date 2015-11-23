a9842934b6c73ae18b5be9fd3f092f21e005f10c
Alex Zai <azai91@gmail.com>
2015-11-22 22:46:16 -0800

    (feat) lint

--- src/config.py
+++ src/config.py
@@ -1,3 +1,9 @@
+"""
+Configurations and constants
+"""
+
+# pylint: disable=relative-import
+
 from workflow import ICON_ACCOUNT, ICON_EJECT, ICON_WARNING, ICON_SYNC
 
 CLIENT_ID = '3162'
@@ -6,10 +12,13 @@ SCOPE = 'make_payments access_friends'
 REDIRECT_URI = 'http://127.0.0.1:1337'
 CACHE_MAX_AGE = 60 * 60 * 24 # one day
 
-AUTH_URL= 'https://api.venmo.com/v1/oauth/authorize?client_id=%s&scope=%s&response_type=code&redirect_uri=%s' % (CLIENT_ID, SCOPE, REDIRECT_URI)
+AUTH_URL = 'https://api.venmo.com/v1/oauth/authorize?client_id=%s&scope=\
+            %s&response_type=code&redirect_uri=%s' % (CLIENT_ID, SCOPE, REDIRECT_URI)
 TOKEN_URL = 'https://api.venmo.com/v1/oauth/access_token'
-FRIENDS_URL = 'https://api.venmo.com/v1/users/%s/friends?access_token=%s&limit=1000' # need to change limit
-PAYMENTS_URL = 'https://api.venmo.com/v1/payments?access_token=%s&user_id=%s&note=%s&amount=%s&audience=%s'
+FRIENDS_URL = 'https://api.venmo.com/v1/users/%s/friends?access_token=\
+              %s&limit=1000' # need to change limit
+PAYMENTS_URL = 'https://api.venmo.com/v1/payments?access_token=%s&user_id=\
+               %s&note=%s&amount=%s&audience=%s'
 
 LOGIN = {
     'title' : 'Login',
