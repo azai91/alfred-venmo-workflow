90618eed30fcf4438b6a772ea5f4aae3adf6b939
Alex Zai <azai91@gmail.com>
2015-11-22 21:48:02 -0800

    (feat) lint

--- tests/util_test.py
+++ tests/util_test.py
@@ -7,13 +7,23 @@ import src.util as util
 import unittest
 
 class TestUtil(unittest.TestCase):
+    """
+    Unit test util library
+    """
 
     def test_validate_amount(self):
-        self.assertEqual(util.validate_amount('1'),'1.00')
-        self.assertEqual(util.validate_amount('10'),'10.00')
-        self.assertEqual(util.validate_amount('10.'),'10.00')
-        self.assertEqual(util.validate_amount('10.0'),'10.00')
-        self.assertEqual(util.validate_amount('10.00'),'10.00')
+        """
+        Test validate_amount function in util library
+
+        validate_amount returns properly formatted currency or False if string formmated
+        improperly
+        """
+
+        self.assertEqual(util.validate_amount('1'), '1.00')
+        self.assertEqual(util.validate_amount('10'), '10.00')
+        self.assertEqual(util.validate_amount('10.'), '10.00')
+        self.assertEqual(util.validate_amount('10.0'), '10.00')
+        self.assertEqual(util.validate_amount('10.00'), '10.00')
         self.assertFalse(util.validate_amount('-'))
         self.assertFalse(util.validate_amount('f'))
         self.assertFalse(util.validate_amount('1f'))
@@ -22,28 +32,42 @@ class TestUtil(unittest.TestCase):
 
 
     def test_create_post_message(self):
+        """
+        Test create_post_message function in util library
+
+        create_post_message takes a properly formatted json string and return formatted
+        string for push notification
+        """
+
         sample_friend = json.dumps({
             'display_name' : 'Test',
             'amount' : '12.00',
             'note' : 'test'
         })
-        self.assertEqual(str(util.create_post_message(sample_friend)),'Test received $12.00 for test')
+        self.assertEqual(str(util.create_post_message(sample_friend)),
+                         'Test received $12.00 for test')
 
         sample_friend = json.dumps({
             'display_name' : 'Test',
             'amount' : '-12.00',
             'note' : 'test'
         })
-        self.assertEqual(str(util.create_post_message(sample_friend)),'Test was charged $12.00 for test')
+        self.assertEqual(str(util.create_post_message(sample_friend)),
+                         'Test was charged $12.00 for test')
 
     def test_format_amount(self):
-        self.assertEqual(util.format_amount('1'),'$1.00')
-        self.assertEqual(util.format_amount('-1'),'$1.00')
-        self.assertEqual(util.format_amount('1.'),'$1.00')
-        self.assertEqual(util.format_amount('-1.'),'$1.00')
-        self.assertEqual(util.format_amount('1.0'),'$1.00')
-        self.assertEqual(util.format_amount('-1.0'),'$1.00')
+        """
+        Test format_amount function in util library
 
+        format_amount converts string to dollar formatted string
+        """
+
+        self.assertEqual(util.format_amount('1'), '$1.00')
+        self.assertEqual(util.format_amount('-1'), '$1.00')
+        self.assertEqual(util.format_amount('1.'), '$1.00')
+        self.assertEqual(util.format_amount('-1.'), '$1.00')
+        self.assertEqual(util.format_amount('1.0'), '$1.00')
+        self.assertEqual(util.format_amount('-1.0'), '$1.00')
 
 
 if __name__ == '__main__':
